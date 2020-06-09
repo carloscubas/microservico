c90d94d299dcb7e11106028f1e2900b96d7d064c
s2it_csilva <s2it_csilva@uolinc.com>
2020-06-09 14:58:08 -0300

    inclusao do log no service

--- ecommerce/src/main/java/com/example/ecommerce/services/CheckoutServices.java
+++ ecommerce/src/main/java/com/example/ecommerce/services/CheckoutServices.java
@@ -1,5 +1,7 @@
 package com.example.ecommerce.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,8 @@ import com.example.ecommerce.repository.SalesRepository;
 
 @Service
 public class CheckoutServices {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(CheckoutServices.class);
 	
 	private static final String URL = "http://localhost:8090/stock/updateStock";
 	
